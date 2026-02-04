bc9ac330b46e83b9236bcefca477191b10848fdf
Rishi Babu <rishibabu.rajesh@bridgelabz.com>
2026-02-04 10:49:45 +0530

    UC4: Render oops as banner using String Array and Loop.

--- oopsBannerApp.java
+++ oopsBannerApp.java
@@ -1,11 +1,16 @@
 public class oopsBannerApp {
     public static void main(String[] args) {
-        System.out.println(String.join("", " ***** ", "   "," ***** ", "   ", " ***** ", "   ", " ***** "));
-        System.out.println(String.join("", "*     *", "   ","*     *", "   ", "*     *", "   ", "*      "));
-        System.out.println(String.join("", "*     *", "   ","*     *", "   ", "*     *", "   ", "*      "));
-        System.out.println(String.join("", "*     *", "   ","*     *", "   ", "****** ", "   ", " ***** "));
-        System.out.println(String.join("", "*     *", "   ","*     *", "   ", "*      ", "   ", "      *"));
-        System.out.println(String.join("", "*     *", "   ","*     *", "   ", "*      ", "   ", "      *"));
-        System.out.println(String.join("", " ***** ", "   "," ***** ", "   ", "*      ", "   ", " ***** "));
+        String[] lists = new String[7];
+        lists[0] = String.join("", " ***** ", "   "," ***** ", "   ", " ***** ", "   ", " ***** ");
+        lists[1] = String.join("", "*     *", "   ","*     *", "   ", "*     *", "   ", "*      ");
+        lists[2] = String.join("", "*     *", "   ","*     *", "   ", "*     *", "   ", "*      ");
+        lists[3] = String.join("", "*     *", "   ","*     *", "   ", "****** ", "   ", " ***** ");
+        lists[4] = String.join("", "*     *", "   ","*     *", "   ", "*      ", "   ", "      *");
+        lists[5] = String.join("", "*     *", "   ","*     *", "   ", "*      ", "   ", "      *");
+        lists[6] = String.join("", " ***** ", "   "," ***** ", "   ", "*      ", "   ", " ***** ");
+
+        for (String line : lists) {
+            System.out.println(line);
+        }
     }
 }
